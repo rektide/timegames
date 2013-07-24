14319f7ecd55b8d010d1f02dd7141b6ed3a56b9f
rektide <rektide@voodoowarez.com>
2013-07-24 01:59:27 -0400

    Capture last publish date.

--- timegames.js
+++ timegames.js
@@ -1,4 +1,5 @@
 var sax= require("sax"),
+  isodate= require("isodate"),
   http= require("http"),
   Q= require("q")
 
@@ -8,8 +9,29 @@ if(process.argv.length != 3){
 
 var name= process.argv[2]
 
-
 function getLastPublished(){
+	var sax= sax.parser(true),
+	  publishedDefer= Q.defer()
+	sax.onopentag= function(node){
+		if(node == "published"){
+			sax.ontext= function(t){
+				sax.close()
+				try{
+					var time= isodate(t)
+					httpDefer.resolve(t)
+				}catch(ex){
+					httpDefer.reject(ex)
+				}
+			}
+		}
+	}
+	var req= http.request("https://github.com/"+name+".atom",function(res){
+		res.setEncoding("utf8")
+		res.on("data",function(chunk){
+			sax.write(chunk)
+		})
+	})
+	return publishDefer.promise
 }
 
 function getTimestamp(){
