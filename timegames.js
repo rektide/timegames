51c8d3e22aad9a38647c9cae4e222e181949bb46
rektide <rektide@voodoowarez.com>
2013-07-24 01:25:50 -0400

    Skeleton project.

---
+++ timegames.js
@@ -0,0 +1,22 @@
+var sax= require("sax"),
+  http= require("http"),
+  Q= require("q")
+
+if(process.argv.length != 3){
+	console.error("Incorrect arguments")
+}
+
+var name= process.argv[2]
+
+
+function getLastPublished(){
+}
+
+function getTimestamp(){
+}
+
+function writeTimestamp(){
+}
+
+function commit(){
+}
